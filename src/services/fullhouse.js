4810bd5c37222c0f6beb1817c76b45821bee28e8
André Farias <andrerfarias@gmail.com>
2020-06-17 15:12:26 -0300

    removing comments

--- src/services/fullhouse.js
+++ src/services/fullhouse.js
@@ -1,14 +1,10 @@
 
 export const findCombos = (workingCombo, currentIndex, remainingItems, sourceArray, combos) => {
-  // LOOP OVER THE REMAINING ELEMENTS TO BE CHECKED
   for (let sourceIndex = currentIndex; sourceIndex < sourceArray.length; sourceIndex++) {
-    // NEXT COMBO POSSIBILITY.
     const comboPossibility = [ ...workingCombo, sourceArray[sourceIndex] ];
-    // IF COMBO IS OF RIGHT LENGTH (COMBO IS FOUND)
     if (remainingItems === 1) { 
       combos.push(comboPossibility);
     } else {
-      //IF NOT GO CHECK AND THE NEXT INDEX IN COMBINATION
       combos = findCombos(comboPossibility, sourceIndex + 1, remainingItems - 1, sourceArray, combos);
     }
   }
@@ -18,7 +14,6 @@ export const findCombos = (workingCombo, currentIndex, remainingItems, sourceArr
 const generateCombinations = (sourceArray, comboLength) => {
   const sourceLength = sourceArray.length;
   if (comboLength > sourceLength) return [];
-  // RECURSIVELY FIND COMBOS
   return findCombos([], 0, comboLength, sourceArray, []);
 }
 
@@ -27,7 +22,6 @@ export const fullHouseCombinations = (cards) => {
   let twoCombinations = {};
   const fullHouseCombinations = [];
 
-  // SEPARATING COMBOS
   cards.forEach(card => {
     const matches = cards.filter(match => match.value === card.value);
     if (matches.length > 2){
@@ -38,17 +32,14 @@ export const fullHouseCombinations = (cards) => {
     }
   });
   
-  // BUILDING POSSIBLE COMBOS
   Object.keys(threeCombinations)
     .forEach(key => {
       const suitableTwoCombinations = Object
         .keys(twoCombinations)
         .filter(twoCombKey => twoCombKey !== key)
       
-      //CHECKING IF HAS SUITABLE COMBOS THAT ARE NOT FROM THE SAME SUIT
       if (suitableTwoCombinations.length === 0) return
 
-      //IF HAS COMBINATIONS OF TWO COMBINE BOTH THREE AND TOW COMBOS 
       suitableTwoCombinations.forEach(twoKey => {
         const allThreeCombinations = generateCombinations(threeCombinations[key].map(card => card.code), 3);
         const allTwoCombinations = generateCombinations(twoCombinations[twoKey].map(card => card.code), 2);
